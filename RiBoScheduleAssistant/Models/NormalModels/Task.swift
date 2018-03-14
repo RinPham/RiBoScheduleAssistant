c51ad4fb2c0a5c85e17dc084d81224f7a2f1db2e
Rin Pham <rinhd01@gmail.com>
2018-03-14 09:35:28 +0700

    - add realm models and normal models - add task object

---
+++ RiBoScheduleAssistant/Models/NormalModels/Task.swift
@@ -0,0 +1,19 @@
+//
+//  Task.swift
+//  RiBoScheduleAssistant
+//
+//  Created by Rin Pham on 3/14/18.
+//  Copyright © 2018 Rin Pham. All rights reserved.
+//
+
+import Foundation
+
+struct Task {
+    
+    var id: String
+    var title: String
+    var time: Date
+    var des: String
+    var isDone: Bool
+    
+}
---
+++ RiBoScheduleAssistant/Models/RealmModels/RTask.swift
@@ -0,0 +1,19 @@
+//
+//  RTask.swift
+//  RiBoScheduleAssistant
+//
+//  Created by Rin Pham on 3/14/18.
+//Copyright © 2018 Rin Pham. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RTask: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
